fe17f418b3f1bf3470ac564aafb3c80d4841d763
AndreyCorpse28090 <280990corpse@gmail.com>
2024-03-24 12:47:05 +0300

    Done

--- MainForm.cs
+++ MainForm.cs
@@ -12,9 +12,43 @@ namespace DZ1_Bukov
 {
     public partial class MainForm : Form
     {
+        private bool isTextChanged = false;
         public MainForm()
         {
             InitializeComponent();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!isTextChanged)
+            {
+                label3.Text = "Текст любой";
+                isTextChanged = true;
+            }
+            else
+            {
+                label3.Text = "Любой текст";
+                isTextChanged = false;
+            }           
+        }
+
+        private void Exit_click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult reply = MessageBox.Show(
+               "Вы действительно хотите выйти?", "Выход",
+               MessageBoxButtons.YesNo, MessageBoxIcon.Question
+           );
+            if (reply == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
